aefad808ae7f4931dcd1d0a9947dfc09ad339f7a
Johnathan <johnathan@mistyrobotics.com>
2019-12-02 13:36:21 -0700

    First commit

---
+++ C#/Sample Projects/IntroSkills/Simulator/SimulatedMisty.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using MistyRobotics.Common;
+using MistyRobotics.SDK.Commands;
+using MistyRobotics.SDK.Events;
+using MistyRobotics.SDK.Responses;
+using MistyRobotics.Common.Types;
+using MistyRobotics.SDK;
+using MistyRobotics.Common.Data;
+
+namespace Simulator
+{
+	/// <summary>
+	/// Example event and command processing simulator to test the skills
+	/// Uses MockRobot to create a simulated environment for testing skills without a robot
+	/// Needs further implementation for proper testing
+	/// </summary>
+	public class SimulatedMisty
+	{
+		public IMockRobot MockedRobot { get; }
+
+		private IEventDetails _locomotionCommandEventDetails;
+		private IEventDetails _haltCommandEventDetails;
+		private IEventDetails _audioPlayCompleteCommandEventDetails;
+
+		private bool _keyPhraseRecRunning = false;
+		private bool _faceRecRunning = false;
+		private bool _faceDetectRunning = false;
+
+		//TODO Temp hack to avoid multi events
+		private bool _keyPhraseRecRegistered = false;
+		private bool _faceRecRegistered = false;
+		private bool _capTouchRegistered = false;
+		private bool _bumpRegistered = false;
+
+		/// <summary>
+		/// Starts a simulated misty to simulate command responses and events using the MockRobot
+		/// </summary>
+		/// <param name="skill">Instance of your skill</param>
+		/// <param name="logLevel">Skill Log Level to start in</param>
+		/// <param name="publishLoggerMessages">Indicates that the system should register for log messages for more information</param>
+		/// <param name="loggingPreface">Overwrite default logging preface to help label runs in log</param>
+		public SimulatedMisty(IMistySkill skill, SkillLogLevel logLevel = SkillLogLevel.Verbose, bool publishLoggerMessages = false, string loggingPreface = null)
+		{
+			//Get the mocked robot
+			MockedRobot = new MockRobot(skill, logLevel, loggingPreface);
+
+			MockedRobot.SetManualEvent(EventType.AudioPlayComplete);
+			MockedRobot.SetManualEvent(EventType.TouchSensor);
+			MockedRobot.SetManualEvent(EventType.BumpSensor);
+			MockedRobot.SetManualEvent(EventType.KeyPhraseRecognized);
+			MockedRobot.SetManualEvent(EventType.FaceRecognition);
+
+			//Setup command listener
+			MockedRobot.RobotCommandSent += HandleRobotCommand;
+
+			//Setup Event Listeners
+			MockedRobot.EventRegistered += HandleEventRegistered;
+
+			//Prepare callbacks and events
+			PrepareCommandMockedResponses();
+			PrepareActuatorEventResponses();
+			PrepareTOFEventResponses();
+
+			if (publishLoggerMessages)
+			{
+				((IMockLogger)MockedRobot.SkillLogger).SDKLogMessageTriggered += PrintMessage;
+			}
+		}
+
+		/// <summary>
+		/// Write all the messages from the SDK to the debug console so we can monitor actions
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="exception"></param>
+		private void PrintMessage(object sender, LogMessage message)
+		{
+			Debug.WriteLine(message.Message);
+		}
+
+		/// <summary>
+		/// Populate the command response queue to return the data for these requests in the order added
+		/// </summary>
+		private void PrepareCommandMockedResponses()
+		{
+			//These modes will cause the command to return these items for every request
+			MockedRobot.SetCommandQueueMode(MessageType.GetAudioList, QueueMode.RepeatLastUntilNew);
+			MockedRobot.SetCommandQueueMode(MessageType.GetImageList, QueueMode.RepeatLastUntilNew);
+
+			MockedRobot.SetNextResponse(MessageType.GetAudioList, new GetAudioListResponse(new List<AudioDetails> { new AudioDetails { Name = "Test.wav", SystemAsset = false } }));
+			MockedRobot.SetNextResponse(MessageType.GetImageList, new GetImageListResponse(new List<ImageDetails> { new ImageDetails { Name = "Test.jpg", SystemAsset = false }, new ImageDetails { Name = "Test2.jpg", SystemAsset = false } }));
+		}
+
+		/// <summary>
+		/// Populate the event response queue with fake Actuator events
+		/// </summary>
+		private void PrepareActuatorEventResponses()
+		{
+
+			//This will set the queue to send the next command in the queue until it gets to the last one.  
+			//If there is only one in the queue, it will repeatedly send it until more items are added
+			MockedRobot.SetEventQueueMode(EventType.ActuatorPosition, QueueMode.RepeatLastUntilNew);
+			
+			MockedRobot.SetNextEvent(EventType.ActuatorPosition, new MockEvent { RobotEvent = ActuatorEvent.GetMockEvent(30, ActuatorPosition.HeadPitch, -1) });
+			MockedRobot.SetNextEvent(EventType.ActuatorPosition, new MockEvent { RobotEvent = ActuatorEvent.GetMockEvent(45, ActuatorPosition.HeadPitch, -1) });
+			MockedRobot.SetNextEvent(EventType.ActuatorPosition, new MockEvent { RobotEvent = ActuatorEvent.GetMockEvent(20, ActuatorPosition.HeadYaw, -1) });
+			MockedRobot.SetNextEvent(EventType.ActuatorPosition, new MockEvent { RobotEvent = ActuatorEvent.GetMockEvent(10, ActuatorPosition.HeadPitch, -1) });
+			MockedRobot.SetNextEvent(EventType.ActuatorPosition, new MockEvent { RobotEvent = ActuatorEvent.GetMockEvent(30, ActuatorPosition.HeadPitch, -1) });
+		}
+
+		/// <summary>
+		/// Populate the event response queue with fake TimeOfFlight events
+		/// </summary>
+		private void PrepareTOFEventResponses()
+		{
+			//This will set the queue to repeatedly cycle through the current events for Time Of Flight types
+			MockedRobot.SetEventQueueMode(EventType.TimeOfFlight, QueueMode.Cycle);
+
+			MockedRobot.SetNextEvent(EventType.TimeOfFlight, new MockEvent { RobotEvent = TimeOfFlightEvent.GetMockEvent(TimeOfFlightPosition.FrontCenter, .6, TimeOfFlightType.Range, 0, -1) });
+			MockedRobot.SetNextEvent(EventType.TimeOfFlight, new MockEvent { RobotEvent = TimeOfFlightEvent.GetMockEvent(TimeOfFlightPosition.FrontCenter, .5, TimeOfFlightType.Range, 0, -1) });
+			MockedRobot.SetNextEvent(EventType.TimeOfFlight, new MockEvent { RobotEvent = TimeOfFlightEvent.GetMockEvent(TimeOfFlightPosition.FrontCenter, .4, TimeOfFlightType.Range, 0, -1) });
+			MockedRobot.SetNextEvent(EventType.TimeOfFlight, new MockEvent { RobotEvent = TimeOfFlightEvent.GetMockEvent(TimeOfFlightPosition.FrontCenter, .3, TimeOfFlightType.Range, 0, -1) });
+			MockedRobot.SetNextEvent(EventType.TimeOfFlight, new MockEvent { RobotEvent = TimeOfFlightEvent.GetMockEvent(TimeOfFlightPosition.FrontCenter, .2, TimeOfFlightType.Range, 0, -1) });
+			MockedRobot.SetNextEvent(EventType.TimeOfFlight, new MockEvent { RobotEvent = TimeOfFlightEvent.GetMockEvent(TimeOfFlightPosition.FrontCenter, .3, TimeOfFlightType.Range, 0, -1) });
+		}
+
+
+		/// <summary>
+		/// Callback when an event is registered so we can setup simulated events
+		/// Under development
+		/// </summary>
+		/// <param name="eventDetails"></param>
+		/// <param name="command"></param>
+		private void HandleEventRegistered(object sender, RegisterEventDetails details)
+		{
+			//TODO Use the event details to multi real register with specific filters
+			var eventDetails = details.EventDetails;
+
+			switch (eventDetails.EventType)
+			{
+				//Based on the event that was registered, then setup an event to be triggered when the commands come in
+				case EventType.LocomotionCommand:
+					_locomotionCommandEventDetails = eventDetails;
+					break;
+				case EventType.HaltCommand:
+					_haltCommandEventDetails = eventDetails;
+					break;
+				case EventType.AudioPlayComplete:
+					_audioPlayCompleteCommandEventDetails = eventDetails;
+					break;
+
+				//Record event id we registered under for manually managed events so we can send events under that id
+				case EventType.TouchSensor:
+					if (_capTouchRegistered) return;
+					_capTouchRegistered = true;
+					//Startup Cap Touch events...
+					_ = Task.Run(async () =>
+					{
+						await Task.Delay(8000);
+						MockedRobot.TriggerRobotEvent(CapTouchEvent.GetMockEvent(true, CapTouchPosition.Chin, eventDetails.EventId));
+						await Task.Delay(1000);
+						MockedRobot.TriggerRobotEvent(CapTouchEvent.GetMockEvent(false, CapTouchPosition.Chin, eventDetails.EventId));
+
+						await Task.Delay(3000);
+						MockedRobot.TriggerRobotEvent(CapTouchEvent.GetMockEvent(true, CapTouchPosition.Chin, eventDetails.EventId));
+						await Task.Delay(1000);
+						MockedRobot.TriggerRobotEvent(CapTouchEvent.GetMockEvent(false, CapTouchPosition.Chin, eventDetails.EventId));
+					});
+					break;
+				case EventType.KeyPhraseRecognized:
+					if (_keyPhraseRecRegistered) return;
+					_keyPhraseRecRegistered = true;
+					_ = Task.Run(async () =>
+					{
+						await Task.Delay(5000);
+						if (_keyPhraseRecRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new KeyPhraseRecognizedEvent(50, eventDetails.EventId));
+						}
+						await Task.Delay(7000);
+						if (_keyPhraseRecRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new KeyPhraseRecognizedEvent(70, eventDetails.EventId));
+						}
+						await Task.Delay(5000);
+						if (_keyPhraseRecRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new KeyPhraseRecognizedEvent(80, eventDetails.EventId));
+						}
+						await Task.Delay(4000);
+						if (_keyPhraseRecRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new KeyPhraseRecognizedEvent(70, eventDetails.EventId));
+						}
+					});
+					break;
+				case EventType.BumpSensor:
+					if (_bumpRegistered) return;
+					_bumpRegistered = true;
+					_ = Task.Run(async () =>
+					{
+						await Task.Delay(10000);
+						MockedRobot.TriggerRobotEvent(BumpSensorEvent.GetMockEvent(true, BumpSensorPosition.FrontLeft, eventDetails.EventId));
+						await Task.Delay(3000);
+						MockedRobot.TriggerRobotEvent(BumpSensorEvent.GetMockEvent(true, BumpSensorPosition.FrontRight, eventDetails.EventId));
+
+						await Task.Delay(5000);
+						MockedRobot.TriggerRobotEvent(BumpSensorEvent.GetMockEvent(false, BumpSensorPosition.FrontLeft, eventDetails.EventId));
+						await Task.Delay(1000);
+						MockedRobot.TriggerRobotEvent(BumpSensorEvent.GetMockEvent(false, BumpSensorPosition.FrontRight, eventDetails.EventId));
+					});
+					break;
+				case EventType.FaceRecognition:
+					if (_faceRecRegistered) return;
+					_faceRecRegistered = true;
+					_ = Task.Run(async () =>
+					{
+						await Task.Delay(5000);
+						if (_faceRecRunning || _faceDetectRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new FaceRecognitionEvent(5, 1, 1, _faceRecRunning ? "brad" : "unknown_person", 2, eventDetails.EventId));
+						}
+						await Task.Delay(2000);
+						if (_faceRecRunning || _faceDetectRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new FaceRecognitionEvent(5, 1, 1, _faceRecRunning ? "joe" : "unknown_person", 2, eventDetails.EventId));
+						}
+
+						await Task.Delay(4000);
+
+						if (_faceRecRunning || _faceDetectRunning)
+						{
+							MockedRobot.TriggerRobotEvent(new FaceRecognitionEvent(5, 1, 1, "unknown_person", 2, eventDetails.EventId));
+						}
+					});
+					break;
+			}
+
+		}
+
+		/// <summary>
+		/// When certain commands are trigered, we might want to do something else like trigger an event
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="callback"></param>
+		private async void HandleRobotCommand(object sender, RobotCommandDetails details)
+		{
+			var command = details.RobotCommand;
+			switch (command.MessageType)
+			{
+				//These commands trigger events if the skill is regisered
+				case MessageType.Stop:
+					if (_locomotionCommandEventDetails != null)
+					{
+						MockedRobot.TriggerRobotEvent(new LocomotionCommandEvent(0, 0, _locomotionCommandEventDetails.EventId));
+					}
+					break;
+				case MessageType.Drive:
+					if (_locomotionCommandEventDetails != null)
+					{
+						DriveCommand newCommand = (DriveCommand)command;
+						MockedRobot.TriggerRobotEvent(new LocomotionCommandEvent(newCommand.LinearVelocity, newCommand.AngularVelocity, _locomotionCommandEventDetails.EventId));
+					}
+					break;
+				case MessageType.DriveTime:
+					if (_locomotionCommandEventDetails != null)
+					{
+						DriveTimeCommand newCommand = (DriveTimeCommand)command;
+						MockedRobot.TriggerRobotEvent(new LocomotionCommandEvent(newCommand.LinearVelocity, newCommand.AngularVelocity, _locomotionCommandEventDetails.EventId));
+					}
+					break;
+				case MessageType.Halt:
+					if (_haltCommandEventDetails != null)
+					{
+						HaltCommand newCommand = (HaltCommand)command;
+						MockedRobot.TriggerRobotEvent(new HaltCommandEvent(newCommand.MotorMasks, _haltCommandEventDetails.EventId));
+					}
+					break;
+				case MessageType.PlayAudio:
+					if (_audioPlayCompleteCommandEventDetails != null)
+					{
+						PlayAudioCommand newCommand = (PlayAudioCommand)command;
+						await Task.Delay(3000);  //send an event 3 seconds after started playing for now...
+						MockedRobot.TriggerRobotEvent(new AudioPlayCompleteEvent(newCommand.FileName, _audioPlayCompleteCommandEventDetails.EventId));
+					}
+					break;
+
+				//Update state flag so know if events should really be sent
+				case MessageType.StartKeyPhraseRecognition:
+					_keyPhraseRecRunning = true;
+					break;
+				case MessageType.StopKeyPhraseRecognition:
+					_keyPhraseRecRunning = false;
+					break;
+				case MessageType.StartFaceDetection:
+					_faceDetectRunning = true;
+					break;
+				case MessageType.StartFaceRecognition:
+					_faceRecRunning = true;
+					break;
+				case MessageType.StopFaceDetection:
+					_faceDetectRunning = false;
+					break;
+				case MessageType.StopFaceRecognition:
+					_faceRecRunning = false;
+					break;
+			}
+		}
+
+	}
+}
---
+++ C#/Sample Projects/IntroSkills/SkillLibrary/HelloWorldSkill.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using MistyRobotics.Common;
+using MistyRobotics.Common.Data;
+using MistyRobotics.Common.Types;
+using MistyRobotics.SDK;
+using MistyRobotics.SDK.Messengers;
+
+namespace SkillLibrary
+{
+	/// <summary>
+	/// Skill to demonstrate basic robot commands
+	/// Assumes defaults assets are on the robot, update the names of assets if they have changed
+	/// </summary>
+	public class HelloWorldSkill : IMistySkill
+	{
+		/// <summary>
+		/// Robot reference
+		/// </summary>
+		private IRobotMessenger _misty;
+
+		/// <summary>
+		/// Flag indicating class was disposed
+		/// </summary>
+		private bool _disposed = false;
+
+		#region Required Skill Methods, Event Handlers & Accessors
+
+		/// <summary>
+		/// Skill details for the robot
+		/// Currently you need a guid to distinguish your skill from others on the robot, get one at this link and paste it in
+		/// https://www.guidgenerator.com/online-guid-generator.aspx
+		/// 
+		/// There are other parameters you can set if you want:
+		///   Description - a description of your skill
+		///   TimeoutInSeconds - timeout of skill in seconds
+		///   StartupRules - a list of options to indicate if a skill should start immediately upon startup
+		///   BroadcastMode - different modes can be set to share different levels of information from the robot using the 'SkillData' websocket
+		///   AllowedCleanupTimeInMs - How long to wait after calling OnCancel before denying messages from the skill and performing final cleanup  
+		/// </summary>
+		public INativeRobotSkill Skill { get; private set; } = new NativeRobotSkill("HelloWorldSkill", "98ed9ccf-e84c-4b67-bd80-b6d8c1efc785")
+		{
+			TimeoutInSeconds = 60 * 1  //times out in 1 minute, but runs quicker than that
+		};
+
+		/// <summary>
+		///	This method is called by the wrapper to set your robot interface
+		///	You need to save this off in the local variable commented on above as you are going use it to call the robot
+		/// </summary>
+		/// <param name="robotInterface"></param>
+		public void LoadRobotConnection(IRobotMessenger robotInterface)
+		{
+			_misty = robotInterface;
+		}
+		
+		/// <summary>
+		/// Called when the robot wants to start this skill
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void OnStart(object sender, IDictionary<string, object> parameters)
+		{
+			try
+			{
+				//These calls assume system assets on the robot at the time of writing. 
+				//Update as needed for new or different assets or as an exercise to allow user to pass in asset names :)
+				_misty.PlayAudio("s_Acceptance.wav", 100, null);	
+				_misty.MoveHead(45, 25, 0, 50, AngularUnit.Degrees, null);
+				_misty.MoveArms(0, 45, 25, 60, null, AngularUnit.Degrees, null);
+				_misty.ChangeLED(0, 255, 0, null);
+				_misty.DisplayImage("e_Disoriented.jpg", 1, null);
+
+				//Pause for 4 seconds, if the cancellation token is set during this time, exit the pause and the method
+				if (!_misty.Wait(4000)) { return; }
+
+				_misty.PlayAudio("s_Awe2.wav", 100, null);
+				_misty.MoveHead(75, 15, 30, 50, AngularUnit.Degrees, null);
+				_misty.MoveArms(-45, 0, 60, 100, null, AngularUnit.Degrees, null);
+				_misty.ChangeLED(0, 255, 255, null);
+				_misty.DisplayImage("e_ContentRight.jpg", 1, null);
+
+				//Pause for 3.5 seconds, if the cancellation token is set during this time, exit the pause and the method
+				if (!_misty.Wait(3500)) { return; }
+
+				_misty.PlayAudio("s_Joy.wav", 100, null);
+				_misty.MoveHead(75, 25, 10, 50, AngularUnit.Degrees, null);
+				_misty.MoveArms(-45, 45, 60, 100, null, AngularUnit.Degrees, null);
+				_misty.ChangeLED(255, 255, 255, null);
+				_misty.DisplayImage("e_ContentLeft.jpg", 1, null);
+
+				//Pause for 2.5 seconds, if the cancellation token is set during this time, exit the pause and the method
+				if (!_misty.Wait(2500)) { return; }
+
+				_misty.PlayAudio("s_PhraseHello.wav", 100, null);
+				_misty.MoveHead(-10, 0, -10, 50, AngularUnit.Degrees, null);
+				_misty.MoveArms(0, -45, 60, 100, null, AngularUnit.Degrees, null);
+				_misty.ChangeLED(0, 0, 255, null);
+				_misty.DisplayImage("e_Joy.jpg", 1, null);
+
+				//Tell the robot the skill has completed early
+				_misty.SkillCompleted();  
+			}
+			catch (Exception ex)
+			{
+				_misty.SkillLogger.Log($"HelloWorldSkill : OnStart: => Exception", ex);
+			}
+		}
+
+		/// <summary>
+		/// Called when the skill is cancelled
+		/// </summary>
+		public void OnCancel(object sender, IDictionary<string, object> parameters)
+		{
+			_misty?.SkillLogger.LogInfo($"HelloWorldSkill : OnCancel called");
+		}
+
+		/// <summary>
+		/// Called when the skill times out
+		/// </summary>
+		public void OnTimeout(object sender, IDictionary<string, object> parameters)
+		{
+			_misty?.SkillLogger.LogInfo($"HelloWorldSkill : OnTimeout called");
+		}
+
+		/// <summary>
+		/// OnPause simply calls OnCancel in this example
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void OnPause(object sender, IDictionary<string, object> parameters)
+		{
+			//In this example, Pause is not implemented by default and it simply calls OnCancel
+			_misty.SkillLogger.LogVerbose($"HelloWorldSkill : OnPause called");
+			OnCancel(sender, parameters);
+		}
+
+		/// <summary>
+		/// OnResume simply calls OnStart in this example
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void OnResume(object sender, IDictionary<string, object> parameters)
+		{
+			//In this example, Resume is not implemented by default and it simply calls OnStart
+			_misty.SkillLogger.LogVerbose($"HelloWorldSkill : OnResume called");
+			OnStart(sender, parameters);
+		}
+
+		/// <summary>
+		/// Dispose method must be implemented by the class
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Protected Dispose implementation
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_disposed)
+			{
+				return;
+			}
+
+			if (disposing)
+			{
+				// Free any other managed objects here.
+			}
+
+			// Free any unmanaged objects here.
+			_disposed = true;
+		}
+
+		/// <summary>
+		/// Skill Finalizer/Destructor
+		/// </summary>
+		~HelloWorldSkill()
+		{
+			Dispose(false);
+		}
+		
+		#endregion
+	}
+}
