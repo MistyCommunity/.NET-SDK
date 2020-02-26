929e5f6d04fdfab85f347e59332c9527134bb94c
Adam Citarella <adam.citarella@mistyrobotics.com>
2020-02-26 16:51:03 -0700

    Adding license text to navigation skill

--- C#/Sample Projects/Navigation/SharedTypes/SkillHelper.cs
+++ C#/Sample Projects/Navigation/SharedTypes/SkillHelper.cs
@@ -1,9 +1,17 @@
-﻿//////////////////////////////////////////////////////////////////////////////////////////
-//
-// This is sample prototype quality Misty skill code that represents one way to achieve certain
-// functionality. Use as you wish.
-//
-//////////////////////////////////////////////////////////////////////////////////////////
+﻿/******************************************************************************
+*    Copyright 2020 Misty Robotics, Inc.
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+******************************************************************************/
 
 using System;
 using System.Collections.Concurrent;
