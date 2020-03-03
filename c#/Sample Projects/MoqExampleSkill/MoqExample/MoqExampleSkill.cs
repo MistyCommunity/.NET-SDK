61c9777050379da91ce58a2232e1d0d87ede160f
brad-allen <bradley.m.allen@gmail.com>
2020-03-02 17:13:19 -0700

    Started example of moqing skills

---
+++ c#/Sample Projects/MoqExampleSkill/MoqExample/MoqExampleSkill.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MistyRobotics.Common;
+using MistyRobotics.SDK.Commands;
+using MistyRobotics.Common.Data;
+using MistyRobotics.SDK.Events;
+using MistyRobotics.SDK.Responses;
+using MistyRobotics.Common.Types;
+using MistyRobotics.SDK;
+using MistyRobotics.SDK.Messengers;
+
+namespace MoqExampleTask
+{
+	public sealed class MoqExampleSkill : IMistySkill
+	{
+		/// <summary>
+		/// Make a local variable to hold the misty robot interface, call it whatever you want 
+		/// </summary>
+		private IRobotMessenger _misty;
+
+		/// <summary>
+		/// Skill details for the robot
+		/// 
+		/// There are other parameters you can set if you want:
+		///   Description - a description of your skill
+		///   TimeoutInSeconds - timeout of skill in seconds
+		///   StartupRules - a list of options to indicate if a skill should start immediately upon startup
+		///   BroadcastMode - different modes can be set to share different levels of information from the robot using the 'SkillData' websocket
+		///   AllowedCleanupTimeInMs - How long to wait after calling OnCancel before denying messages from the skill and performing final cleanup  
+		/// </summary>
+		public INativeRobotSkill Skill { get; private set; } = new NativeRobotSkill("MoqExampleTask", "e60853e9-55cd-408b-9343-8cb746527289");
+
+		/// <summary>
+		///	This method is called by the wrapper to set your robot interface
+		///	You need to save this off in the local variable commented on above as you are going use it to call the robot
+		/// </summary>
+		/// <param name="robotInterface"></param>
+		public void LoadRobotConnection(IRobotMessenger robotInterface)
+		{
+			_misty = robotInterface;
+		}
+
+		/// <summary>
+		/// This event handler is called when the robot/user sends a start message
+		/// The parameters can be set in the Skill Runner (or as json) and used in the skill if desired
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void OnStart(object sender, IDictionary<string, object> parameters)
+		{
+			//TODO Put your code here and update the summary above
+			_misty.ChangeLED(255, 36, 0, OnResponse);
+		}
+
+		/// <summary>
+		/// This event handler is called when Pause is called on the skill
+		/// User can save the skill status/data to be retrieved when Resume is called
+		/// Infrastructure to help support this still under development, but users can implement this themselves as needed for now 
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void OnPause(object sender, IDictionary<string, object> parameters)
+		{
+			//In this template, Pause is not implemented by default
+		}
+
+		/// <summary>
+		/// This event handler is called when Resume is called on the skill
+		/// User can restore any skill status/data and continue from Paused location
+		/// Infrastructure to help support this still under development, but users can implement this themselves as needed for now 
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void OnResume(object sender, IDictionary<string, object> parameters)
+		{
+			//TODO Put your code here and update the summary above
+		}
+		
+		/// <summary>
+		/// This event handler is called when the cancel command is issued from the robot/user
+		/// You currently have a few seconds to do cleanup and robot resets before the skill is shut down... 
+		/// Events will be unregistered for you 
+		/// </summary>
+		public void OnCancel(object sender, IDictionary<string, object> parameters)
+		{
+			//TODO Put your code here and update the summary above
+		}
+
+		/// <summary>
+		/// This event handler is called when the skill timeouts
+		/// You currently have a few seconds to do cleanup and robot resets before the skill is shut down... 
+		/// Events will be unregistered for you 
+		/// </summary>
+		public void OnTimeout(object sender, IDictionary<string, object> parameters)
+		{
+			//TODO Put your code here and update the summary above
+		}
+
+		public void OnResponse(IRobotCommandResponse response)
+		{
+			Debug.WriteLine("Response: " + response.ResponseType.ToString());
+		}
+
+		#region IDisposable Support
+		private bool _isDisposed = false;
+
+		private void Dispose(bool disposing)
+		{
+			if (!_isDisposed)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects).
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				_isDisposed = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~MistyNativeSkill() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
---
+++ c#/Sample Projects/MoqExampleSkill/UnitTest/UnitTest.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MistyRobotics.Common.Types;
+using MistyRobotics.SDK.Messengers;
+using MistyRobotics.SDK.Responses;
+using Moq;
+using MoqExampleTask;
+using Windows.ApplicationModel.Background;
+
+namespace UnitTest
+{
+	public class MockCommandResponse : IRobotCommandResponse
+	{
+		public ResponseStatus Status { get; set; }
+		public string ErrorMessage { get; set; }
+		public MessageType ResponseType { get; }
+	}
+
+
+	[TestClass]
+    public class UnitTest1
+    {
+		[TestMethod]
+        public void TestMethod1()
+		{
+			MoqExampleSkill moqExampleSkill = new MoqExampleSkill();
+			Mock<IRobotMessenger> robotMessengerMock = new Mock<IRobotMessenger>();
+			IRobotCommandResponse changeLEDResponse = new RobotCommandResponse();
+			robotMessengerMock.Setup
+			(
+				x => x.ChangeLEDAsync
+				(
+					It.IsAny<uint>(), 
+					It.IsAny<uint>(), 
+					It.IsAny<uint>()
+				)
+			).Returns(Task.Factory.StartNew(() => changeLEDResponse).AsAsyncOperation());
+
+			var test = new Action<IRobotCommandResponse>(moqExampleSkill.OnResponse);
+
+			robotMessengerMock.Setup
+			(
+				x => x.ChangeLED
+				(
+					It.IsAny<uint>(),
+					It.IsAny<uint>(),
+					It.IsAny<uint>(),
+					It.IsAny<ProcessCommandResponse>()
+				)
+			).Callback(() => moqExampleSkill.OnResponse(changeLEDResponse));
+			
+			moqExampleSkill.LoadRobotConnection(robotMessengerMock.Object);
+			
+			moqExampleSkill.OnStart(this, null);
+
+			Task.WaitAll(Task.Delay(5000));
+
+			moqExampleSkill.OnCancel(this, null);
+		}
+
+		private void X_RobotCommandEventReceived(object sender, MistyRobotics.SDK.Events.IRobotCommandEvent e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
